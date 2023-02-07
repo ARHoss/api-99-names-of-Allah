ca4c4c5a609fa857b79b79cd36e21e880397976c
arhoss <rhossaincse@gmail.com>
2023-02-07 11:39:04 -0700

    changes made to adjust with cyclic

--- server.js
+++ server.js
@@ -32,6 +32,11 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true})
         db = client.db('Islamic-info')
         // Database collection name
         collectionName = db.collection('99-names-of-Allah')
+
+        // Listener on port provided by the environment or 3000
+        app.listen(process.env.PORT || PORT, function() {
+            console.log('listening on '+PORT)
+        })
 })
 .catch(console.error);
 
@@ -85,13 +90,11 @@ app.get('/api/:order', (req,res) => {
     
 })
 
+
 // Listener on port provided by the environment or 3000
-app.listen(process.env.PORT || PORT, function() {
-    console.log('listening on '+PORT)
-})
-
-
-
+// app.listen(process.env.PORT || PORT, function() {
+//     console.log('listening on '+PORT)
+// })
 
 // create
 
